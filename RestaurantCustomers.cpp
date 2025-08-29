f401c251f2bcad9b83ff605f92f39178bad46e54
youessaitch <ayushkumar0077680@gmail.com>
2025-08-30 02:55:26 +0530

    restaurant customers

---
+++ RestaurantCustomers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ll n;
+    cin >> n;
+    vector<pair<ll, ll>> v(n);
+    multimap<ll, ll> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        ll a, b;
+        cin >> a >> b;
+        v.push_back({a, b});
+        mpp.insert({a, 1});  // arrival
+        mpp.insert({b, -1}); // departure
+    }
+    ll maxi = 0;
+    ll cnt = 0;
+    for (auto it = mpp.begin(); it != mpp.end(); it++)
+    {
+        cnt += it->second;
+        maxi = max(maxi, cnt);
+    }
+    cout << maxi << endl;
+    return 0;
+}
